8e0971964b45d6887cb4e54654c8f5537e5bd672
JaimeIngenia <jamoncayop@unal.edu.co>
2024-01-15 13:38:51 -0500

    ejemplo

---
+++ Controllers/ReservaController.cs
@@ -0,0 +1,113 @@
+﻿using CinemaComfamaVs5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaComfamaVs5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservaController : ControllerBase
+    {
+        private readonly DBCINEMA3Context _context;
+        public ReservaController(DBCINEMA3Context context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Reservas
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Reserva>>> GetReservas()
+        //{
+        //    return await _context.Reservas.ToListAsync();
+        //}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reserva>>> GetReservas()
+        {
+            var reservas = await _context.Reservas
+                .Include(r => r.IdUsuarioNavigation) // Incluye la información de Usuario
+                //.Include(r => r.IdHorarioNavigation) // Incluye la información de Horario
+                .ToListAsync();
+
+            return reservas;
+        }
+
+        // GET: api/Reservas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reserva>> GetReserva(int id)
+        {
+            var reserva = await _context.Reservas.FindAsync(id);
+
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return reserva;
+        }
+
+        // POST: api/Reservas
+        [HttpPost]
+        public async Task<ActionResult<Reserva>> PostReserva(Reserva reserva)
+        {
+            _context.Reservas.Add(reserva);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReserva", new { id = reserva.IdReserva }, reserva);
+        }
+
+        // PUT: api/Reservas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReserva(int id, Reserva reserva)
+        {
+            if (id != reserva.IdReserva)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reserva).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Reservas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReserva(int id)
+        {
+            var reserva = await _context.Reservas.FindAsync(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservas.Remove(reserva);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReservaExists(int id)
+        {
+            return _context.Reservas.Any(e => e.IdReserva == id);
+        }
+
+
+    }
+}
