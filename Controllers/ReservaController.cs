577963f18b56e54331fda5957e65b1777ae25cfd
JaimeIngenia <jamoncayop@unal.edu.co>
2024-03-15 16:35:19 -0500

    swagger master

--- Controllers/ReservaController.cs
+++ Controllers/ReservaController.cs
@@ -1,119 +1,4 @@
-<<<<<<< HEAD
-﻿using CinemaComfamaVs5.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace CinemaComfamaVs5.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ReservaController : ControllerBase
-    {
-        private readonly DBCINEMA3Context _context;
-        public ReservaController(DBCINEMA3Context context)
-        {
-            _context = context;
-        }
-
-
-        // GET: api/Reservas
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Reserva>>> GetReservas()
-        //{
-        //    return await _context.Reservas.ToListAsync();
-        //}
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Reserva>>> GetReservas()
-        {
-            var reservas = await _context.Reservas
-                .Include(r => r.IdUsuarioNavigation) // Incluye la información de Usuario
-                //.Include(r => r.IdHorarioNavigation) // Incluye la información de Horario
-                .ToListAsync();
-
-            return reservas;
-        }
-
-        // GET: api/Reservas/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Reserva>> GetReserva(int id)
-        {
-            var reserva = await _context.Reservas.FindAsync(id);
-
-            if (reserva == null)
-            {
-                return NotFound();
-            }
-
-            return reserva;
-        }
-
-        // POST: api/Reservas
-        [HttpPost]
-        public async Task<ActionResult<Reserva>> PostReserva(Reserva reserva)
-        {
-            _context.Reservas.Add(reserva);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetReserva", new { id = reserva.IdReserva }, reserva);
-        }
-
-        // PUT: api/Reservas/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutReserva(int id, Reserva reserva)
-        {
-            if (id != reserva.IdReserva)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(reserva).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ReservaExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // DELETE: api/Reservas/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteReserva(int id)
-        {
-            var reserva = await _context.Reservas.FindAsync(id);
-            if (reserva == null)
-            {
-                return NotFound();
-            }
-
-            _context.Reservas.Remove(reserva);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ReservaExists(int id)
-        {
-            return _context.Reservas.Any(e => e.IdReserva == id);
-        }
-
-
-    }
-}
-=======
-﻿using CinemaComfamaVs5.Models;
+using CinemaComfamaVs5.Models;
 using CinemaComfamaVs5.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -129,7 +14,7 @@ namespace CinemaComfamaVs5.Controllers
 
         public ReservaController(DBCINEMA3Context context)
         {
-            
+
             _DBContext = context;
 
         }
@@ -194,38 +79,38 @@ namespace CinemaComfamaVs5.Controllers
                     {
                         IdUsuario = r.IdUsuarioNavigation.IdUsuario,
                         IdTipoDocumento = r.IdUsuarioNavigation.IdTipoDocumento ?? 0,
-        
+
                         NumeroDocumento = r.IdUsuarioNavigation.NumeroDocumento ?? "",
                         Nombres = r.IdUsuarioNavigation.Nombres ?? "",
                         Apellidos = r.IdUsuarioNavigation.Apellidos ?? "",
                         Correo = r.IdUsuarioNavigation.Correo ?? "",
                         IdTipoRol = r.IdUsuarioNavigation.IdTipoRol ?? 0
-   
+
                     },
                     Horario = new HorarioDetalleViewModel
                     {
                         IdHorario = r.IdHorarioNavigation.IdHorario,
                         Pelicula = new PeliculaViewModel
                         {
-                       
+
                             IdPelicula = r.IdHorarioNavigation.IdPeliculaNavigation != null ? r.IdHorarioNavigation.IdPeliculaNavigation.IdPelicula : 0,
                             Titulo = r.IdHorarioNavigation.IdPeliculaNavigation != null ? r.IdHorarioNavigation.IdPeliculaNavigation.Titulo : "",
                             ImagenPromocional = r.IdHorarioNavigation.IdPeliculaNavigation != null ? r.IdHorarioNavigation.IdPeliculaNavigation.ImagenPromocional : "",
 
-                            
+
                         },
                         Sala = new SalaViewModel
                         {
-                            
+
                             IdSala = r.IdHorarioNavigation.IdSalaNavigation != null ? r.IdHorarioNavigation.IdSalaNavigation.IdSala : 0,
                             NombreSala = r.IdHorarioNavigation.IdSalaNavigation != null ? r.IdHorarioNavigation.IdSalaNavigation.NombreSala : "",
                             Estado = r.IdHorarioNavigation.IdSalaNavigation != null ? r.IdHorarioNavigation.IdSalaNavigation.Estado : "",
-  
+
 
                         },
                         Hora = new HoraViewModel
                         {
-                  
+
                             IdHora = r.IdHorarioNavigation.IdHoraNavigation != null ? r.IdHorarioNavigation.IdHoraNavigation.IdHora : 0,
                             Hora1 = r.IdHorarioNavigation.IdHoraNavigation != null ? r.IdHorarioNavigation.IdHoraNavigation.Hora1 ?? TimeSpan.Zero : TimeSpan.Zero,
 
@@ -371,4 +256,3 @@ namespace CinemaComfamaVs5.Controllers
 
     }
 }
->>>>>>> master
